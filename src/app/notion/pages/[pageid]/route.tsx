847cc9843bd9287e032cefca1637093079c0e3b6
useradiw <adiwsnu@gmail.com>
2023-12-08 20:57:39 +0700

    notion to md

--- src/app/notion/pages/[pageid]/route.tsx
+++ src/app/notion/pages/[pageid]/route.tsx
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { Client } from "@notionhq/client";
+import { NotionToMarkdown } from "notion-to-md";
 
 const secret = process.env.NOTION_SECRET;
 
@@ -9,10 +10,14 @@ export async function GET( req: Request, { params }: { params: { pageid: string}
 
     try {
         const notion = new Client({auth: secret})
-        const query = await notion.blocks.children.list({
-            block_id: pageid,
-        })
-        return NextResponse.json({body: query}, {status: 200})
+        // const query = await notion.blocks.children.list({
+        //     block_id: pageid,
+        // })
+        // return NextResponse.json({body: query}, {status: 200})
+        const n2m = new NotionToMarkdown({ notionClient: notion });
+        const mdblocks = await n2m.pageToMarkdown(pageid);
+        const mdString = n2m.toMarkdownString(mdblocks);
+        return NextResponse.json({body: mdString.parent}, {status: 200});
     } catch (error) {
         return NextResponse.json({message: "Something went wrong.", error: error}, {status: 400})
     }
--- src/components/display/content.tsx
+++ src/components/display/content.tsx
@@ -4,8 +4,9 @@ interface pageProps {
 const Content = async ({pageid}: pageProps) => {
     const req = await fetch(`http://localhost:3000/notion/pages/${pageid}`, {cache: "no-store"});
     const res = await req.json();
-    const data = res.body.results as blockdata[];
-    console.log(data.map((item) => item.paragraph)[2])
+    // const data = res.body.results as blockdata[];
+    // console.log(data.map((item) => item.paragraph)[2])
+    console.log(res)
     return (
         <>
             <div>Content</div>
