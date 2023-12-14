9fed2371be7cbd4870b340eb0a9c7b337295b75d
useradiw <adiwsnu@gmail.com>
2023-12-14 20:46:08 +0700

    md to react

--- src/components/display/content.tsx
+++ src/components/display/content.tsx
@@ -1,15 +1,19 @@
+import html from "remark-html";
+import remarkGfm from "remark-gfm";
+import emoji from "remark-emoji";
+import Markdown from "react-markdown";
+
 interface pageProps {
     pageid: string,
 }
 const Content = async ({pageid}: pageProps) => {
     const req = await fetch(`http://localhost:3000/notion/pages/${pageid}`, {cache: "no-store"});
     const res = await req.json();
-    // const data = res.body.results as blockdata[];
-    // console.log(data.map((item) => item.paragraph)[2])
-    console.log(res)
     return (
         <>
-            <div>Content</div>
+            <section className="prose">
+                <Markdown remarkPlugins={[remarkGfm, html, emoji]}>{res.body}</Markdown>
+            </section>
         </>
     )
 };
